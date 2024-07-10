7253803d9cb2aa7d7e3b734bbaeeaff4edc88970
aalriskyl <aalriskyl3105@gmail.com>
2024-07-10 11:40:57 +0700

    apalah

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import axiosInstance from './axiosConfig';
+import axiosInstance from '../axiosConfig';
 
 const Login = () => {
   const [formData, setFormData] = useState({
