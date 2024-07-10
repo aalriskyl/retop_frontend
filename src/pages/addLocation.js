d7f30802ee28034391f14d8c1b1e7213100846d3
aalriskyl <aalriskyl3105@gmail.com>
2024-07-10 16:44:49 +0700

    susah banget

--- src/pages/addLocation.js
+++ src/pages/addLocation.js
@@ -161,7 +161,7 @@ const AddLocation = () => {
                   value={tokoData.latitude}
                   onChange={(e) => setTokoData({ ...tokoData, latitude: e.target.value })}
                   required
-                  pattern="^-?[0-9]{1,3}\.[0-9]{1,10}$" // Example pattern for latitude validation
+                  pattern="^-?\d+(\.\d+)?$" // Example pattern for latitude validation
                   title="Latitude must be a number with up to 10 decimal places."
                   className="mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
@@ -176,7 +176,7 @@ const AddLocation = () => {
                   value={tokoData.longitude}
                   onChange={(e) => setTokoData({ ...tokoData, longitude: e.target.value })}
                   required
-                  pattern="^-?[0-9]{1,3}\.[0-9]{1,10}$" // Example pattern for longitude validation
+                  pattern="^-?\d+(\.\d+)?$" // Example pattern for longitude validation
                   title="Longitude must be a number with up to 10 decimal places."
                   className="mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
