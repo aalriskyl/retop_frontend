a04079765bbba5b546c6d9e52a65ad3b7085d2fe
aalriskyl <aalriskyl3105@gmail.com>
2024-07-25 14:29:28 +0700

    ratings

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,67 +1,91 @@
-import React from 'react';
+import React from "react";
 
 const Reviews = () => {
   const reviews = [
     {
       text: "Pelayanan sangat memuaskan! Laptop saya yang rusak bisa diperbaiki dengan cepat dan harganya sangat terjangkau. Terima kasih banyak!",
       author: "Budi Santoso",
-      image: "https://randomuser.me/api/portraits/men/1.jpg"
+      image: "https://randomuser.me/api/portraits/men/1.jpg",
     },
     {
       text: "Saya sangat puas dengan hasil reparasi laptop saya di sini. Tim sangat profesional dan komunikatif. Sangat direkomendasikan!",
       author: "Rina Suryani",
-      image: "https://randomuser.me/api/portraits/women/2.jpg"
+      image: "https://randomuser.me/api/portraits/women/2.jpg",
     },
     {
       text: "Layanan cepat dan ramah! Laptop saya sekarang kembali berfungsi dengan baik setelah mengalami masalah hardware.",
       author: "Ahmad Fadli",
-      image: "https://randomuser.me/api/portraits/men/3.jpg"
+      image: "https://randomuser.me/api/portraits/men/3.jpg",
     },
     {
       text: "Pelayanan cepat dan ramah! Saya sangat puas dengan perbaikan laptop saya.",
       author: "Siti Aminah",
-      image: "https://randomuser.me/api/portraits/women/4.jpg"
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       text: "Harga sangat terjangkau untuk kualitas pelayanan yang diberikan. Sangat puas!",
       author: "Ali Usman",
-      image: "https://randomuser.me/api/portraits/men/5.jpg"
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       text: "Layanan yang diberikan sangat profesional dan memuaskan.",
       author: "Dewi Kusuma",
-      image: "https://randomuser.me/api/portraits/women/6.jpg"
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
     {
       text: "Rekomendasi terbaik untuk perbaikan laptop di kota ini!",
       author: "Joko Priyono",
-      image: "https://randomuser.me/api/portraits/men/7.jpg"
+      image: "https://randomuser.me/api/portraits/men/7.jpg",
     },
     {
       text: "Pelayanan cepat dan hasil perbaikan sangat memuaskan.",
       author: "Lisa Kartika",
-      image: "https://randomuser.me/api/portraits/women/8.jpg"
+      image: "https://randomuser.me/api/portraits/women/8.jpg",
     },
     {
       text: "Tempat terbaik untuk perbaikan laptop dengan harga terjangkau.",
       author: "Agus Prasetyo",
-      image: "https://randomuser.me/api/portraits/men/9.jpg"
-    }
+      image: "https://randomuser.me/api/portraits/men/9.jpg",
+    },
   ];
 
+  const getRandomRating = () => {
+    return (Math.random() * 1 + 4).toFixed(1);
+  };
+
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-slate-200 py-12">
-      <h1 className="text-3xl font-bold mb-8 text-center">Ulasan</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 max-w-7xl mx-auto">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen py-12 bg-slate-200">
+      <h1 className="mb-8 text-3xl font-bold text-center">Ulasan</h1>
+      <div className="grid grid-cols-1 gap-6 px-4 mx-auto sm:grid-cols-2 md:grid-cols-3 max-w-7xl">
         {reviews.map((review, index) => (
-          <div key={index} className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center">
-            <img 
+          <div
+            key={index}
+            className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg"
+          >
+            <img
               src={review.image}
               alt={review.author}
-              className="w-16 h-16 rounded-full mb-4"
+              className="w-16 h-16 mb-4 rounded-full"
             />
-            <p className="text-gray-800 text-center">{review.text}</p>
-            <p className="text-gray-600 mt-2 text-right">- {review.author}</p>
+            <p className="text-center text-gray-800">{review.text}</p>
+            <p className="mt-2 text-right text-gray-600">- {review.author}</p>
+            <div className="flex mt-4">
+              {[...Array(5)].map((_, i) => (
+                <svg
+                  key={i}
+                  className={`w-4 h-4 ${
+                    i < Math.floor(getRandomRating())
+                      ? "text-yellow-400"
+                      : "text-gray-300"
+                  }`}
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.049.927l2.573 5.216 5.75.834-4.168 4.062.984 5.739-5.139-2.699-5.139 2.699.984-5.739L.726 6.977l5.75-.834L9.049.927z" />
+                </svg>
+              ))}
+            </div>
+            <p className="mt-2 text-gray-600">{getRandomRating()} stars</p>
           </div>
         ))}
       </div>
--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const ShopList = ({ markers, handleShopSelect, selectedShop }) => {
+  const getRandomRating = () => {
+    return (Math.random() * 1 + 4).toFixed(1);
+  };
+
   return (
     <div className="w-full sm:w-1/3 lg:w-full p-4 bg-white rounded-lg shadow-lg border-r border-gray-200 overflow-y-auto max-h-[80vh]">
-      <h2 className="text-2xl font-bold mb-4">Daftar Toko:</h2>
+      <h2 className="mb-4 text-2xl font-bold">Daftar Toko:</h2>
       <ul className="space-y-2 text-lg tracking-wider">
-        {markers.map(marker => (
+        {markers.map((marker) => (
           <li
             key={marker._id}
             onClick={() => handleShopSelect(marker)} // Trigger handleShopSelect with marker
-            className={`cursor-pointer p-4 rounded-md hover:bg-blue-100 shadow-md ${selectedShop?._id === marker._id ? 'bg-blue-200' : ''}`}
+            className={`cursor-pointer p-4 rounded-md hover:bg-blue-100 shadow-md ${
+              selectedShop?._id === marker._id ? "bg-blue-200" : ""
+            }`}
           >
             <div className="font-bold">{marker.name}</div>
             <div>{marker.address}</div>
-
+            <div className="flex mt-2">
+              {[...Array(5)].map((_, i) => (
+                <svg
+                  key={i}
+                  className={`w-4 h-4 ${
+                    i < Math.floor(getRandomRating())
+                      ? "text-yellow-400"
+                      : "text-gray-300"
+                  }`}
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.049.927l2.573 5.216 5.75.834-4.168 4.062.984 5.739-5.139-2.699-5.139 2.699.984-5.739L.726 6.977l5.75-.834L9.049.927z" />
+                </svg>
+              ))}
+            </div>
+            <p className="mt-2 text-gray-600">{getRandomRating()} stars</p>
           </li>
         ))}
       </ul>
